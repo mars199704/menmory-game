a4fcd0d413d8096374cd5e5ea05801a8d31ebcd9
edward <mingyu0416@outlook.com>
2021-04-07 22:04:28 +0800

    finish the start button & click event

--- all.js
+++ all.js
@@ -7,19 +7,31 @@ const start = document.querySelector('.start')
 
 //  check if its correct
 let CheckCorrect = []
+//  the place which can't click
+let totalCorrect = []
+let point = 0
 const correct = false
 
 //  start the game
 start.addEventListener('click', function(){
+  start.classList.add('none')
   createGame()
 
   //  cells click events
   const cells = document.querySelectorAll('.cell')
   cells.forEach(cell => {
-    cell.addEventListener('click',openCard)
-  })
-})
+    cell.addEventListener('click',function(){
+      const vm = this
+      openCard(vm, cells)
 
+
+
+      console.log(CheckCorrect, point ,totalCorrect)
+    })
+  })
+
+
+})
 
 //  initial information
 function createGame(){
@@ -27,57 +39,55 @@ function createGame(){
   const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
   const answer = arr.concat(arr).sort(randomSort);
 
-  // //  push cells into grid
-  // for(let i=0;i<row*col;i++){
-  //   const cell = document.createElement("div")
-  //   cell.classList.add('cell')
-  //   grid.appendChild(cell)
-
-  //   const cellFront = document.createElement("div")
-  //   cellFront.classList.add('cellFront')
-  //   cellFront.innerHTML = `${answer[i]}`
-
-  //   const cellBack = document.createElement("div")
-  //   cellBack.classList.add('cellBack')
-
-  //   cell.appendChild(cellFront)
-  //   cell.appendChild(cellBack)
-  // }
   var str = ''
   for(var i=0;i<36;i++){
-    // var cellInformation = `
-    // <div class="cell" id=""${i}>
-    //   <div class="cellFront">${answer[i]}</div>
-    //   <div class="cellBack"></div>
-    // </div>
-    // `
-    var cellInformation = answer[i]
+    var cellInformation = `
+    <div class="cell" id="${i}">
+      <div class="cellFront">${answer[i]}</div>
+      <div class="cellBack"></div>
+    </div>
+    `
+    str += cellInformation
   }
-  str = str + cellInformation
   grid.innerHTML = str
-} 
-
+}
 
 function randomSort(a, b) {
   return Math.random()>.5 ? -1 : 1;
   //用Math.random()函式生成0~1之間的隨機數與0.5比較，返回-1或1
 }
 
-function openCard(){
+function openCard(vm, cells){
+  vm.classList.add('open')
+  CheckCorrect.push({
+    value: vm.innerText,
+    id: vm.id
+  })
+
   if(CheckCorrect.length == 2){
-    CheckCorrect = []
-  }else{
-    this.classList('open')
-    CheckCorrect.push({
-      value: this.innerText,
-      id: Math.random()
-    })
-    // check(CheckCorrect)
+    if(CheckCorrect[1].id == CheckCorrect[0].id){
+      CheckCorrect.pop()
+    }
+    else{
+      check(CheckCorrect, cells)
+      CheckCorrect = []
+    }
   }
 }
 
-// function check(CheckCorrect){
-//   if(CheckCorrect.length == 2 & CheckCorrect[0] !== CheckCorrect[1]){
-    
-//   }
-// }
+function check(CheckCorrect, cells){
+  let value1 = CheckCorrect[0].value
+  let value2 = CheckCorrect[1].value
+  let id1 = CheckCorrect[0].id
+  let id2 = CheckCorrect[1].id
+  if(value1 == value2){
+    point += 1
+    totalCorrect.push(id1, id2)
+    return
+  }else{
+    setTimeout(function() {
+      cells[id1].classList.remove('open')
+      cells[id2].classList.remove('open')
+    }, 1000)
+  }
+}
